728a8b218adeace3d7a47c0bdd25bb3622c725b7
Tomas <tomas.butkevicius99@gmail.com>
2021-05-13 11:44:29 +0300

    remove hardcoded value

--- routes/routes.js
+++ routes/routes.js
@@ -5,35 +5,18 @@ const Ajv = require('ajv')
 const schemaorg = require('schemaorg-jsd')
 var jsonld_request = require('jsonld-request');
 
-const doc =
-{
-    "@context": [
-        "https://www.w3.org/2018/credentials/v1",
-        "https://www.w3.org/2018/credentials/examples/v1"
-    ],
-    "id": "http://example.edu/credentials/3732",
-    "type": ["VerifiableCredential", "UniversityDegreeCredential"],
-    "issuer": "https://example.edu/issuers/14",
-    "issuanceDate": "2010-01-01T19:23:24Z",
-    "expirationDate": "2020-01-01T19:23:24Z",
-    "credentialSubject": {
-        "id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
-        "degree": {
-            "type": "BachelorDegree",
-            "name": "Bachelor of Science and Arts"
-        }
-    },
-    "proof": {}
-};
-
-router.get("/compact", async (req, res) => {
-    const compacted = await jsonld.compact(doc, {});
+router.post("/compact", async (req, res) => {
+    const compacted = await jsonld.compact(req.body, {});
     res.send(compacted);
 });
 
+router.post("/expand", async (req, res) => {
+    const compacted = await jsonld.compact(req.body, {});
+    res.send(await jsonld.expand(compacted));
+});
 
-router.get("/validate", async (req, res) => {
-    const compacted = await jsonld.compact(doc, {});
+router.post("/validate", async (req, res) => {
+    const compacted = await jsonld.compact(req.body, {});
     console.log("Compacted");
     console.log(compacted);
     console.log("END Compacted");
@@ -45,22 +28,21 @@ router.get("/validate", async (req, res) => {
     schema = await getRequest('https://www.w3.org/2018/credentials/v1');
     let isValid = ajv.validate(schema, compacted);
     res.send(isValid);
-
 });
 
 function getRequest(url) {
     return new Promise(function (resolve, reject) {
         jsonld_request(url, function (error, res, body) {
-        if (!error && res.statusCode == 200) {
-            console.log("request body");
-            console.log(body);
-            console.log("END request body");
-          resolve(body);
-        } else {
-          reject(error);
-        }
-      });
+            if (!error && res.statusCode == 200) {
+                console.log("request body");
+                console.log(body);
+                console.log("END request body");
+                resolve(body);
+            } else {
+                reject(error);
+            }
+        });
     });
-  }
-  
+}
+
 module.exports = router;
